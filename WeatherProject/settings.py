4585e1906c811a2c2440a25679069a4b982f34fa
LC-burigo <burigo-jm@hotmail.com>
2020-10-01 13:50:48 -0300

    add WeatherApp.urls

--- WeatherProject/settings.py
+++ WeatherProject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = BASE_DIR/'WeatherApp/templates/WeatherApp'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'WeatherProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
