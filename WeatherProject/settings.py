303319e7994f206fca2a5aab8229c85ddc8b8269
LC-burigo <burigo-jm@hotmail.com>
2020-10-02 18:00:57 -0300

    add css file

--- WeatherProject/settings.py
+++ WeatherProject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR/'WeatherApp/templates/WeatherApp'
-
+STATIC_DIR = BASE_DIR/'WeatherApp/static'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
