03daf82c2dbc527e81f39163b5cc212f5855ed11
LC-burigo <burigo-jm@hotmail.com>
2020-10-03 13:46:29 -0300

    Try to fix the views

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -4,26 +4,33 @@ from pprint import pprint
 import datetime
 from geopy.geocoders import Nominatim
 from .forms import CityForm
-
+from .models import City
 
 def index(request):
     url = "https://community-open-weather-map.p.rapidapi.com/onecall/timemachine"
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        form.save()  # Saving the filled form in database
 
-    form = CityForm()
+    form = CityForm()  # Put the form in the variable form
+
+    cities = City.objects.all()  # Get all the table content in the variable cities
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url, format(city)).json()
 
         Geolocator = Nominatim(user_agent="Lucas")
-        Location = Geolocator.geocode(city)
+        Location = Geolocator.geocode(city.Address)
         Coordinates = []
         Latitude = Location.latitude
         Longitude = Location.longitude
         Coordinates.append(Latitude)
         Coordinates.append(Longitude)
         #############################################################################
-        querystring = {"lat": Coordinates[0], "lon": Coordinates[1], "dt": timestamp}
+        querystring = {"lat": Coordinates[0], "lon": Coordinates[1], "dt": city.Dt}
 
         headers = {
             'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
