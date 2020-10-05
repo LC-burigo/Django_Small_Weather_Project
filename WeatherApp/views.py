4b5c332e2b5fae2ec6a253fcabcfa68acc6a4563
LC-burigo <burigo-jm@hotmail.com>
2020-10-05 11:57:23 -0300

    Try to fix the views

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -8,20 +8,23 @@ from .models import City
 
 
 def index(request):
+    Input_Address = 'london'
     url = "https://community-open-weather-map.p.rapidapi.com/onecall/timemachine"
+    Cities = City.objects.all()
+    form = CityForm()
 
     if request.method == 'POST':
-        form = CityForm(request.POST)
-        form.save()  # Saving the filled form in database
+        form = CityForm(request.POST)  # Handling form request
+        form.save()
 
-    form = CityForm()  # Put the form in the variable form
+        # if form.is_valid():
+        #     Address = form.cleaned_data['Address']
 
-    Cities = City.objects.all()  # Get all the table content in the variable cities
-    Weather_data = []
+    Weather_list = []
 
-    for city in Cities:   # For each line of the database table City
+    for city in Cities:
 
-        # Get the coordinates of address of each database table City
+        # Get the coordinates of address of the city
         Geolocator = Nominatim(user_agent="Lucas")
         Location = Geolocator.geocode(city.Address)
         Coordinates = []
@@ -38,20 +41,26 @@ def index(request):
         }
         # Get all the features of this particular city, in the last 24 hours
         response = requests.request("GET", url, headers=headers, params=querystring)
-        data = response.json()
 
-        # Get only the current features of this particular city and put it in a dictionary
-        city_weather = {
-            'City': city.Address,
-            'Temperature': data['current']['temp'],
-            'Humidity': data['current']['humidity'],
-            'Wind_Speed': data['current']['wind_speed'],
-            'Pressure': data['current']['pressure'],
-        }
+        if response.status_code == 200:
 
-        # Put the dictionary inside of a list (element)
-        Weather_data.append(city_weather)  #
+            data = response.json()
 
-    pprint(Weather_data)
-    context = {'city_weather': Weather_data, 'form': form}
+            # Get only the current features of this particular city and put it in a dictionary
+            City_Weather = {
+                'City': city.Address,
+                'Temperature': data['current']['temp'],
+                'Humidity': data['current']['humidity'],
+                'Wind_Speed': data['current']['wind_speed'],
+                'Pressure': data['current']['pressure'],
+            }
+
+            Weather_list.append(City_Weather)
+
+        else:
+            City_Weather = {}
+            print('Address not found')
+
+    context ={'Weather_list': Weather_list, 'form': form}
+
     return render(request, 'WeatherApp/Weather.html', context)
