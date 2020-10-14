725e55a071c4c8a3c4022eabfd711a043ac69886
LC-burigo <burigo-jm@hotmail.com>
2020-10-13 23:54:23 -0300

    Try to fix the views

--- WeatherApp/urls.py
+++ WeatherApp/urls.py
@@ -6,5 +6,7 @@ app_name = 'WeatherApp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('current', views.Current, name='current'),
-    path('delete/<pk>/', views.WeatherDeleteView.as_view(), name='delete_city')
+    path('delete/<pk>/', views.WeatherDeleteView.as_view(), name='delete_city'),
+    path('hourly', views.Hourly.as_view(), name='hourly'),
+    path('delete_hourly/<pk>/', views.WeatherHourlyDeleteView.as_view(), name='delete_hourly_city'),
 ]
--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -5,7 +5,7 @@ from .forms import CityForm
 from .models import City
 from django.views.generic import DeleteView, TemplateView
 from django.urls import reverse_lazy
-
+import datetime
 
 class IndexView(TemplateView):
     template_name = 'Base.html'
@@ -80,7 +80,7 @@ def Current(request):
         Weather_list.append(City_Weather)
     context = {'Weather_list': Weather_list, 'form': form}
 
-    return render(request, 'WeatherApp/Weather.html', context)
+    return render(request, 'WeatherApp/Current_Weather.html', context)
 
 
 class WeatherDeleteView(DeleteView):
@@ -90,4 +90,75 @@ class WeatherDeleteView(DeleteView):
     success_url = reverse_lazy("WeatherApp:current")
 
 
-# def Hourly(request):
+def Hourly(request):
+    url = "https://community-open-weather-map.p.rapidapi.com/onecall/timemachine"
+    url_second = "https://community-open-weather-map.p.rapidapi.com/weather"
+    form = CityForm()
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)  # Handling form request
+
+        if form.is_valid():
+            New_City = form.cleaned_data['Address']
+            Existing_City = City.objects.filter(Address=New_City).count()
+            if Existing_City == 0:
+                querystring = {"callback": "test", "id": "2172797", "units": "%22metric%22 or %22imperial%22",
+                               "mode": "xml%2C html", "q": New_City}
+
+                headers = {
+                    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+                    'x-rapidapi-key': "6446924734mshd20c29c9014fd63p155d13jsnc1cdd0345c05"
+                }
+
+                response = requests.request("GET", url_second, headers=headers, params=querystring)
+                if response.status_code == 200:
+                    form.save()
+                else:
+                    Error_message = 'City does not exist in the world'
+
+            else:
+                Error_message = 'City already exists in the database'
+
+    Cities = City.objects.all()
+    Weather_Dict = {}
+    Weather_List = []
+
+    for city in Cities:
+
+        # Get the coordinates of address of the city
+        Geolocator = Nominatim(user_agent="Lucas")
+        Location = Geolocator.geocode(city.Address)
+        Coordinates = []
+        Latitude = Location.latitude
+        Longitude = Location.longitude
+        Coordinates.append(Latitude)
+        Coordinates.append(Longitude)
+        #############################################################################
+        querystring = {"lat": Coordinates[0], "lon": Coordinates[1], "dt": city.Dt}
+
+        headers = {
+            'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+            'x-rapidapi-key': "6446924734mshd20c29c9014fd63p155d13jsnc1cdd0345c05"
+        }
+        # Get all the features of this particular city, in the last 24 hours
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        data = response.json()
+        # Get only the hourlies features of this particular city and put it in a dictionary
+
+        hourly = data['hourly']
+        i = 0
+        while i < len(hourly):
+            Weather_Dict[datetime.datetime.fromtimestamp(hourly[i]['dt']).strftime('%Y-%m-%d %H:%M:%S')] = [hourly[1]['temp'], hourly[i]['humidity'], hourly[i]['wind_seed'], hourly[i]['pressure']]
+        Weather_List.append(Weather_Dict)
+
+    context = {'Weather_List': Weather_List, 'form': form}
+
+    return render(request, 'WeatherApp/Hourly_Weather.html', context)
+
+
+class WeatherHourlyDeleteView(DeleteView):
+    model = City
+    context_object_name = "Wd"
+    template_name = "WeatherApp/Weather_hourly_confirm_delete.html"
+    success_url = reverse_lazy("WeatherApp:hourly")
